15f4155a5bee7f343d562204f6e587bf422040a5
axeratos <nikitalukianitcares@gmail.com>
2022-11-01 09:58:48 +0200

    Model and migrations

--- recipes_book/models.py
+++ recipes_book/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=255)
+    recipe_body = models.TextField()
+    dish_image = models.ImageField(upload_to="dish_photos/%Y/%m/%d")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    likes = models.ManyToManyField(User, related_name="likes")
+
+    def __str__(self):
+        return f"{self.recipe_name}"
